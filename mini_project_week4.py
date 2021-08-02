497f9da8dea09cc4727d1db38e660baeb2a46c02
Dirie Isse <dirieisse0@mail.com>
2021-08-02 15:57:47 +0100

    init

--- mini_project_week4.py
+++ mini_project_week4.py
@@ -1,17 +1,23 @@
 import csv 
 
-Order_status="Preparing"
+
 def ordersmenu():
     while True:
-        order = input("Enter(0) to return to main menu\nEnter(1) to show all orders\nEnter(2) to enter order details\nEnter(3) to update existing order status to ready\n")
+        order = input("Enter(0) to return to main menu\nEnter(1) to show all orders\nEnter(2) to enter order details\nEnter(3) to update existing order status to ready\n") 
+
         if order == "0":
             mainmenu()
+
+
+        
         elif order == "1":
             with open("orders.csv", mode= 'r') as file2:                
                  csv_file2 = csv.DictReader(file2)
-                 readingorderslist = list(csv_file2)  #reading the orders csv file into a list of order dictoinaries for user
-                 print(readingorderslist)
+                 for row in csv_file2:
+                     print(row)
             continue
+    
+
         elif order == "2":
             customer_name = input("Please enter your name: ") 
             customer_address = input("Please enter your address: ")
@@ -34,28 +40,31 @@ def ordersmenu():
                 print(readingproductlist1)      #printing out enumerated list of courier dictionaries for user selection
             courier_index_selection = int(input("Please enter a number that corresponds with your chosen courier from the list above: "))
             with open('orders.csv', mode= 'a', newline="") as file_:
+                Order_status="Preparing"
                 fieldnames=['Customer name', 'Customer address','Customer phone','Courier id','Status','Product IDs']
                 writer = csv.DictWriter(file_, fieldnames=fieldnames)
                 if file_.tell() ==0:     #if file is empty write the header/fieldnames
                     writer.writeheader()               
                 writer.writerow({'Customer name':customer_name, 'Customer address':customer_address,'Customer phone':customer_phone_number,'Courier id':courier_index_selection,'Status': Order_status,'Product IDs':intergers})
             continue           # 'write in dictionary keys and match their values with user inputs'
+
+
+
         elif order == "3":
-            with open("orders.csv", mode= 'r') as file3:
-                 csv_file3 = csv.DictReader(file3)
-                 enumrating3=enumerate(csv_file3)
-                 readingorderslist3 = list(enumrating3)
-                 print(readingorderslist3)              #print out enumerated list of order dictionaries for user selection
-            order_index_selection = int(input("Please enter a number that corresponds with your chosen order from the list above: "))
             orderstatus=[]
             with open("orders.csv", mode= 'r') as file4:
                 csv_file4= csv.reader(file4)
                 headers = next(csv_file4)
                 for row in csv_file4:
-                       orderstatus.append(row[4])
+                    try:
+                        name= row[0]
+                        order_status_=row[4]
+                        orderstatus.append((name,order_status_))   
+                    except IndexError:                             #index error would come up for blank lines
+                        pass                                      
                 neworderstatus=list(enumerate(orderstatus))
                 print(neworderstatus)                #read status of all orders into an enumerated list for user to select from
-            order_status_selection=int(input("Please enter THE SAME number that corresponds with the order you want to update: "))
+            order_status_selection=int(input("Please enter a number from the list above that corresponds with the order you want to update: "))
             with open("orders.csv", mode= 'r') as file5:
                 csv_file5=csv.DictReader(file5)
                 readingorderslist5=list(csv_file5)
@@ -67,7 +76,7 @@ def ordersmenu():
                 if file5.tell() ==0:
                     writer.writeheader()
                 writer.writerows(readingorderslist5)       #list with updated order status is written to rows
-                print(f'You have updated the status of the following order:{order_index_selection} to READY')
+                print(f'You have updated the status of the following order:{order_status_selection} to READY')
             continue
         else:
             print("YOU HAVE ENTERED THE WRONG OPTION PLEASE TRY AGAIN")
@@ -76,25 +85,27 @@ def ordersmenu():
 
 
 
-import csv
 def couriermenu():
     while True:
         courier_choice = input("Please select from the following options:\nEnter (0) to return to main menu\nEnter (1) to show all couriers\nEnter (2) to add a new courier\n")
         if courier_choice ==  "0":
             mainmenu()
+        
         elif courier_choice == "1":
             with open("couriers.csv", mode= 'r') as file_:
                 csv_file = csv.DictReader(file_)
-                readingcourierlist = list(csv_file)
-                print(readingcourierlist)
+                for row in csv_file:
+                    print(row)
             continue
+
+
         elif courier_choice == "2":
             courier_name = str(input("PLEASE ADD A COURIER NAME: "))
             courier_phone = int(input("PLEASE ADD A COURIER PHONE NUMBER: "))
             with open('couriers.csv', mode= 'a', newline="") as file_:
                 fieldnames=['Courier name', 'Courier phone']
                 writer = csv.DictWriter(file_, fieldnames=fieldnames)
-                if file_.tell() ==0:
+                if file_.tell() ==0:      #tell file method returns current position of file, if empty then right header
                     writer.writeheader()
                 writer.writerow({'Courier name':courier_name, 'Courier phone':courier_phone})
             continue
@@ -104,19 +115,23 @@ def couriermenu():
         return
 
 
-import csv
-
 def productmenu():
     while True:
         product_choice = input("Please select from the following options:\nEnter (0) to return to main menu\nEnter (1) to show all products\nEnter (2) to add a new product\n")
+
+
         if product_choice ==  "0":
             mainmenu()
+        
+
         elif product_choice == "1":
             with open("products.csv", mode = 'r') as file:
                 csv_file = csv.DictReader(file)
-                readingproductlist = list(csv_file)
-                print(readingproductlist)
+                for row in csv_file:
+                    print(row)
             continue
+
+
         elif product_choice == "2":
             product_name = str(input("PLEASE ADD A PRODUCT NAME: "))
             product_price = float(input("PlEASE ADD THE PRODUCT PRICE: "))
@@ -127,6 +142,8 @@ def productmenu():
                     writer.writeheader()
                 writer.writerow({'Product name':product_name, 'Product price':product_price})
             continue
+
+
         else:
             print("YOU HAVE ENTERED THE WRONG OPTION, PLEASE TRY AGAIN")
             productmenu()
@@ -139,10 +156,6 @@ def mainmenu():
     choice = input()
     if choice == "0":
         print("THANKS FOR YOUR TIME, GOODBYE!")
-        file = open('orders.csv',mode= 'r')
-        file1=open('couriers.csv',mode='r')
-        file2=open('products.csv',mode='r')
-        #ask what this means
         exit()
     elif choice == "1":
         productmenu()
@@ -154,37 +167,3 @@ def mainmenu():
         print("YOU HAVE ENTERED THE WRONG OPTION PLEASE TRY AGAIN")
         mainmenu()
 mainmenu()
-    
-
-#put data in product menu func arg 
-# data = [1,2]
-# productmenu(data)
-
-# # Write the file
-# def write_file(filename):
-#     with open(filename) as file:
-#         print(file)
-
-# # Get's the filename  pathway
-# def get_filname():
-#     answer = input('filname?')
-#     return answer
-
-# user_ans = get_filname()
-
-# write_file(user_ans)
-
-
-    #i= 0 or 1 or 2   for row in mycsv, change row[i]
-            #https://stackoverflow.com/questions/3699927/writing-to-a-particular-cell-using-csv-module-in-python
-            #4 upvotes is solution maybe
-
-
-            # df = (pd.read_csv("orders.csv", usecols=['Status']))
-            # print(df.values.tolist())
-            # order_status_selection=int(input("Please enter a number that correlates with the order status you wish to update: "))
-            # neworderstatus=[order_index_selection][4]
-            # neworderstatus='Ready'
-            # with open("orders.csv", mode="w") as file4:
-            #     writer = csv.DictWriter(file4)
-            #     writer.writerows(neworderstatus)
